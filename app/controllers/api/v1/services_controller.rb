a44fb9ca8e80ba623a3db624680bf152e2c40aa0
Esme Berder <esmeyangli@gmail.com>
2019-07-29 18:31:03 +0800

    added rails api and servicescontroller

---
+++ app/controllers/api/v1/services_controller.rb
@@ -0,0 +1,26 @@
+class Api::V1::ServicesController < Api::V1::BaseController
+
+  def index
+    @services = Service.all
+    # render json: @stories #Just for testing
+  end
+
+  def new
+    @service = Service.new
+  end
+
+  def create
+    @service = Service.new(service_params)
+  end
+
+  private
+
+  def set_service
+    @service = Service.find(params[:id])
+  end
+
+  def service_params
+    params.require(:service).permit(:title, :description, :price)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :services, only: [ :index, :create, :new ]
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
